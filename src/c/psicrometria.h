99bbafeac11172144b6a0463f37e4764f4a27288
Ferdinando <ferdinando.battillocchi@gmail.com>
2026-01-03 17:07:39 +0100

    Add license information to psicrometria.h
    Added licensing information and comments to psicrometria.h

--- src/c/psicrometria.h
+++ src/c/psicrometria.h
@@ -1,3 +1,16 @@
+/*
+ * PsicroAddIn - Libreria Psicrometrica per Excel
+ * Copyright (C) 2024  [Tuo Nome o Nome Utente GitHub]
+ * * Questo programma è software libero: è possibile ridistribuirlo e/o 
+ * modificarlo secondo i termini della licenza GNU General Public 
+ * License come pubblicata dalla Free Software Foundation, versione 3.
+ * * Questo programma è distribuito nella speranza che possa essere utile, 
+ * ma SENZA ALCUNA GARANZIA; senza neppure la garanzia implicita di 
+ * COMMERCIABILITÀ o IDONEITÀ PER UNO SCOPO PARTICOLARE. 
+ * Vedere la licenza GNU General Public License per ulteriori dettagli.
+ * * Dovresti aver ricevuto una copia della licenza GNU General Public License 
+ * insieme a questo programma. In caso contrario, vedi <https://www.gnu.org/licenses/>.
+ */
 #ifndef PSICROMETRIA_H
 #define PSICROMETRIA_H
 
